057427d261d36449887446c55bf3dd7e1674d468
Kirti2254 <kirtibhattarai1@gmail.com>
2022-09-22 10:50:58 +0545

    directory_traversal

---
+++ directory_traversal.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+doindent()
+{
+  
+    j=0;
+    while [ "$j" -lt "$1" ]; do
+      echo -en "  " 
+      j=`expr $j + 1`
+    done
+}
+
+traverse() 
+{
+  
+  indent="$2"
+
+  ls "$1" | while read i
+  do
+    doindent $2
+    if [ -d "$1/$i" ]; then
+      echo "Directory: $1/$i"
+     
+      traverse "$1/$i" `expr $2 + 1`
+    else 
+      echo "File: $1/$i"
+    fi
+  done
+}
+
+if [ -z "$1" ]; then
+  traverse . 0
+else
+  traverse "$1" 0
+fi
